1a868f7a1c8a4aeaa81cb3d22c10c4754c0030ee
IvaSerge <ivaserge@ukr.net>
2021-02-23 09:14:17 +0100

    first commit

---
+++ ChangeWorkset.py
@@ -0,0 +1,125 @@
+import clr
+clr.AddReference('RevitAPI')
+import Autodesk
+from Autodesk.Revit.DB import *
+
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+doc = DocumentManager.Instance.CurrentDBDocument
+
+
+def ProcessList(_func, _list):
+	return map(
+		lambda x: ProcessList(_func, x)
+		if type(x) == list else _func(x), _list)
+
+
+def GetParVal(elem, name):
+	value = None
+	# custom parameter
+	param = elem.LookupParameter(name)
+	# check is it a BuiltIn parameter if not found
+	if not(param):
+		param = elem.get_Parameter(GetBuiltInParam(name))
+
+	# get paremeter Value if found
+	try:
+		storeType = param.StorageType
+		# value = storeType
+		if storeType == StorageType.String:
+			value = param.AsString()
+		elif storeType == StorageType.Integer:
+			value = param.AsDouble()
+		elif storeType == StorageType.Double:
+			value = param.AsDouble()
+		elif storeType == StorageType.ElementId:
+			value = param.AsValueString()
+	except:
+		pass
+	return value
+
+
+def GetBuiltInParam(paramName):
+	builtInParams = System.Enum.GetValues(BuiltInParameter)
+	param = []
+	for i in builtInParams:
+		if i.ToString() == paramName:
+			param.append(i)
+			return i
+
+
+def SetupParVal(elem, name, pValue):
+	global doc
+	# custom parameter
+	param = elem.LookupParameter(name)
+	# check is it a BuiltIn parameter if not found
+	if not(param):
+		try:
+			param = elem.get_Parameter(GetBuiltInParam(name)).Set(pValue)
+		except:
+			pass
+
+	if param:
+		try:
+			param.Set(pValue)
+		except:
+			pass
+	return elem
+
+
+def getByCatAndStrParam(_bic, _bip, _val, _isType):
+	global doc
+	if _isType:
+		fnrvStr = FilterStringEquals()
+		pvp = ParameterValueProvider(ElementId(int(_bip)))
+		frule = FilterStringRule(pvp, fnrvStr, _val, False)
+		filter = ElementParameterFilter(frule)
+		elem = FilteredElementCollector(doc).\
+			OfCategory(_bic).\
+			WhereElementIsElementType().\
+			WherePasses(filter).\
+			ToElements()
+	else:
+		fnrvStr = FilterStringEquals()
+		pvp = ParameterValueProvider(ElementId(int(_bip)))
+		frule = FilterStringRule(pvp, fnrvStr, _val, False)
+		filter = ElementParameterFilter(frule)
+		elem = FilteredElementCollector(doc).\
+			OfCategory(_bic).\
+			WhereElementIsNotElementType().\
+			WherePasses(filter).\
+			ToElements()
+	return elem
+
+
+def getTypeByCatFamType(_bic, _fam, _type):
+	global doc
+	fnrvStr = FilterStringEquals()
+
+	pvpType = ParameterValueProvider(ElementId(int(BuiltInParameter.SYMBOL_NAME_PARAM)))
+	pvpFam = ParameterValueProvider(ElementId(int(BuiltInParameter.ALL_MODEL_FAMILY_NAME)))
+
+	fruleF = FilterStringRule(pvpFam, fnrvStr, _fam, False)
+	filterF = ElementParameterFilter(fruleF)
+
+	fruleT = FilterStringRule(pvpType, fnrvStr, _type, False)
+	filterT = ElementParameterFilter(fruleT)
+
+	filter = LogicalAndFilter(filterT, filterF)
+
+	elem = FilteredElementCollector(doc).\
+		OfCategory(_bic).\
+		WhereElementIsElementType().\
+		WherePasses(filter).\
+		FirstElement()
+	return elem
+
+
+# TransactionManager.Instance.EnsureInTransaction(doc)
+
+# TransactionManager.Instance.TransactionTaskDone()
+
+OUT = None
