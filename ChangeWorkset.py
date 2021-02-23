93db930f986fdc765a5058b7c7340c0617b26ae0
IvaSerge <ivaserge@ukr.net>
2021-02-23 09:40:02 +0100

    get Worksets

--- ChangeWorkset.py
+++ ChangeWorkset.py
@@ -118,8 +118,31 @@ def getTypeByCatFamType(_bic, _fam, _type):
 	return elem
 
 
+workset_list = list()
+workset_list.append("00_Ebenen und Raster")
+workset_list.append("01_LINK_Architektur")
+workset_list.append("01_LINK_CAD")
+workset_list.append("01_LINK_Elektro")
+workset_list.append("01_LINK_Heizunk_Kälte")
+workset_list.append("01_LINK_Lüftung")
+workset_list.append("01_LINK_Sanitär")
+workset_list.append("440_Elektro_Anlagen_Starkstrom")
+workset_list.append("440_Elektro_Kabeltrassen Starkstrom")
+workset_list.append("450_Elektro_Labeltrassen_Schwachstrom")
+workset_list.append("450_Eletro_Anlagen_Schwachstrom")
+
+workset_rvt = [
+	i for i in FilteredWorksetCollector(doc) if
+	i.Name in workset_list]
+
+# chekc if all standard worksets exists in project
+if len(workset_rvt) != len(workset_list):
+	raise ValueError('Check worksets!')
+
+# RVT model objects
+
 # TransactionManager.Instance.EnsureInTransaction(doc)
 
 # TransactionManager.Instance.TransactionTaskDone()
 
-OUT = None
+OUT = workset_rvt
