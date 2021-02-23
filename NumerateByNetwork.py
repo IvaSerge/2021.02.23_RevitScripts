2b2bdb8dc49594c11870eeeb9439e9ea2705bcdc
IvaSerge <ivaserge@ukr.net>
2021-02-23 09:44:19 +0100

    PEP8

--- NumerateByNetwork.py
+++ NumerateByNetwork.py
@@ -36,38 +36,36 @@ def SetpParVal(elem, name, pValue):
 def getNextObj(_startObj):
 	global objectList
 	global wireIdList
-	_outlist = list()
 	objectListId = [i.Id for i in objectList]
-	#получение провода из стартового объекта
+	# получение провода из стартового объекта
 	startObjId = _startObj.Id
 	conList = [i for i in _startObj.MEPModel.ConnectorManager.Connectors]
-	#Тут надо будет добавить проверку в какой именно сети находится 
-	#семейство, а также то, что это электрический коннектор
-	#В данный момент проверка упускается - в семействе только один
-	#электрический коннектор
-	
-	
+	# Тут надо будет добавить проверку в какой именно сети находится
+	# семейство, а также то, что это электрический коннектор
+	# В данный момент проверка упускается - в семействе только один
+	# электрический коннектор
+
 	conRefs = conList[0].AllRefs
 	wire = None
 	for con in conRefs:
 		conOwner = con.Owner
 		ownerCategory = conOwner.Category.Id.IntegerValue
 		wireCat = -2008039
-		#проверка, чтоб был найден новый провод
+		# проверка, чтоб был найден новый провод
 		if ownerCategory == wireCat and conOwner.Id not in wireIdList:
 			wireIdList.append(conOwner.Id)
 			wire = conOwner
-	
-	#получение из провода следующего объетка
+
+	# получение из провода следующего объетка
 	if not (wire):
 		return None
-	
+
 	wireId = wire.Id
 	wireCons = wire.ConnectorManager.Connectors
 	allWireRefs = list()
 	for con in wireCons:
 		map(lambda x: allWireRefs.append(x.Owner), con.AllRefs)
-	
+
 	for ref in allWireRefs:
 		refId = ref.Id
 		isNextElem = (refId != wireId) and (refId != startObjId)
@@ -75,24 +73,23 @@ def getNextObj(_startObj):
 			return ref
 	return None
 
+
 reLoad = IN[0]
-#получение начального номера
+# получение начального номера
 startNumber = IN[2]
 
-#выбор стартового объекта
+# выбор стартового объекта
 startObj = UnwrapElement(IN[1])
 # startObj = uidoc.Selection.PickObjects(
 		# Autodesk.Revit.UI.Selection.ObjectType.Element,
 		# "Selection of start element")
 
-
-
 objectList = list()
 wireIdList = list()
 objectList.append(startObj)
 
-#Поиск следующего объекта. Продолжать до тех пор, пока есть следующий объект.
-#Самый первый объект и есть следующий объект.
+# Поиск следующего объекта. Продолжать до тех пор, пока есть следующий объект.
+# Самый первый объект и есть следующий объект.
 nextObj = startObj
 
 while nextObj:
@@ -101,15 +98,14 @@ while nextObj:
 		objectList.append(nextObj)
 
 
-#нумерация
-numList = [i[0]+startNumber for i in enumerate (objectList)]
+# нумерация
+numList = [i[0] + startNumber for i in enumerate(objectList)]
 
-#Запись номера в семейство
+# Запись номера в семейство
 TransactionManager.Instance.EnsureInTransaction(doc)
 for obj, num in zip(objectList, numList):
 	SetpParVal(obj, "MC Object Variable 1", str(num))
 
 TransactionManager.Instance.TransactionTaskDone()
-
 
 OUT = numList
