ad17439719c7f91e9092a50dc5fceda32433bbf8
IvaSerge <ivaserge@ukr.net>
2021-02-23 08:47:51 +0100

    PEP8

--- Plankopf_CopyInfo.py
+++ Plankopf_CopyInfo.py
@@ -4,7 +4,7 @@ import Autodesk
 from Autodesk.Revit.DB import *
 
 import sys
-#sys.path.append(r"C:\Program Files\Dynamo 0.8")
+# sys.path.append(r"C:\Program Files\Dynamo 0.8")
 pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
 sys.path.append(pyt_path)
 
@@ -18,8 +18,10 @@ from RevitServices.Transactions import TransactionManager
 
 doc = DocumentManager.Instance.CurrentDBDocument
 
+
 def ProcessList(_func, _list):
-	return map( lambda x: ProcessList(_func, x) if type(x)==list else _func(x), _list )
+	return map(lambda x: ProcessList(_func, x) if type(x) == list else _func(x), _list)
+
 
 def Unwrap(_item):
 	if isinstance(_item, list):
@@ -27,33 +29,35 @@ def Unwrap(_item):
 	else:
 		return UnwrapElement(_item)
 
+
 def GetParVal(elem, name):
-	#Параметр пользовательский
+	# Параметр пользовательский
 	try:
 		param = elem.LookupParameter(name)
 		storeType = param.StorageType
 		if storeType == StorageType.String:
 			value = elem.LookupParameter(name).AsString()
 		elif storeType == StorageType.Integer:
-			value  = elem.LookupParameter(name).AsDouble()
+			value = elem.LookupParameter(name).AsDouble()
 		elif storeType == StorageType.Double:
 			value = elem.LookupParameter(name).AsDouble()
-	#Параметр встроенный
+	# Параметр встроенный
 	except:
 		bip = GetBuiltInParam(name)
 		storeType = elem.get_Parameter(bip).StorageType
 		if storeType == StorageType.String:
 			value = elem.get_Parameter(bip).AsString()
 		elif storeType == StorageType.Integer:
-			value  = elem.get_Parameter(bip).AsDouble()
+			value = elem.get_Parameter(bip).AsDouble()
 		elif storeType == StorageType.Double:
 			value = elem.get_Parameter(bip).AsDouble()
 	return value
 
+
 def GetBuiltInParam(paramName):
 	builtInParams = System.Enum.GetValues(BuiltInParameter)
 	param = []
-	
+
 	for i in builtInParams:
 		if i.ToString() == paramName:
 			param.append(i)
@@ -62,12 +66,14 @@ def GetBuiltInParam(paramName):
 			continue
 	return param[0]
 
+
 def SetpParVal(elem, name, pValue):
 	global doc
 	elem.LookupParameter(name).Set(pValue)
 	return elem
 
-def getByCatAndStrParam (_bic, _bip, _val, _isType):
+
+def getByCatAndStrParam(_bic, _bip, _val, _isType):
 	global doc
 	if _isType:
 		fnrvStr = FilterStringEquals()
@@ -91,28 +97,28 @@ def getByCatAndStrParam (_bic, _bip, _val, _isType):
 			ToElements()
 	return elem
 
-def getTypeByCatFamType (_bic, _fam, _type):
+
+def getTypeByCatFamType(_bic, _fam, _type):
 	global doc
 	fnrvStr = FilterStringEquals()
-	
+
 	pvpType = ParameterValueProvider(ElementId(int(BuiltInParameter.SYMBOL_NAME_PARAM)))
 	pvpFam = ParameterValueProvider(ElementId(int(BuiltInParameter.ALL_MODEL_FAMILY_NAME)))
-	
+
 	fruleF = FilterStringRule(pvpFam, fnrvStr, _fam, False)
 	filterF = ElementParameterFilter(fruleF)
-	
+
 	fruleT = FilterStringRule(pvpType, fnrvStr, _type, False)
 	filterT = ElementParameterFilter(fruleT)
-	
+
 	filter = LogicalAndFilter(filterT, filterF)
-	
-	elem = FilteredElementCollector(doc).\
-	OfCategory(_bic).\
-	WhereElementIsElementType().\
-	WherePasses(filter).\
-	FirstElement()
-	
+
+	elem = FilteredElementCollector(doc).OfCategory(_bic).\
+		WhereElementIsElementType().\
+		WherePasses(filter).\
+		FirstElement()
 	return elem
+
 
 ElemFrom = Unwrap(IN[0])
 ElemFrom_Sheet = doc.GetElement(ElemFrom.OwnerViewId)
@@ -123,31 +129,30 @@ reload = IN[2]
 pList_Sheet = list()
 pList_Title = list()
 ignorParamsList = [
-		"SHEET_NAME",
-		"VIEWER_SHEET_NUMBER",
-		"SHEET_NUMBER",
-		"VIEW_TYPE",
-		"ELEM_FAMILY_AND_TYPE_PARAM",
-		"VIEW_FAMILY_AND_TYPE_SCHEDULES",
-		"ALL_MODEL_FAMILY_NAME",
-		"VIEW_FAMILY_SCHEDULES",
-		"VIEW_FAMILY",
-		"VIEW_FAMILY_SCHEDULES",
-		"SYMBOL_NAME_PARAM",
-		"ELEM_TYPE_PARAM",
-		"VIEW_TYPE_SCHEDULES",
-		"SHEET_FILE_PATH"
-		]
+	"SHEET_NAME",
+	"VIEWER_SHEET_NUMBER",
+	"SHEET_NUMBER",
+	"VIEW_TYPE",
+	"ELEM_FAMILY_AND_TYPE_PARAM",
+	"VIEW_FAMILY_AND_TYPE_SCHEDULES",
+	"ALL_MODEL_FAMILY_NAME",
+	"VIEW_FAMILY_SCHEDULES",
+	"VIEW_FAMILY",
+	"VIEW_FAMILY_SCHEDULES",
+	"SYMBOL_NAME_PARAM",
+	"ELEM_TYPE_PARAM",
+	"VIEW_TYPE_SCHEDULES",
+	"SHEET_FILE_PATH"]
 
 ScheetBic = "OST_Sheets"
 ScheetNum = "SHEET_NUMBER"
 
-#=========Params for sheets
+# =========Params for sheets
 paramList_Sheet = ElemFrom_Sheet.Parameters
 for param in paramList_Sheet:
 	storeType = param.StorageType
 	if storeType == StorageType.String:
-		#check if parameter Built-in
+		# check if parameter Built-in
 		parBIP = param.Definition.BuiltInParameter.ToString()
 		if parBIP == "INVALID":
 			checkName = param.Definition.Name
@@ -158,23 +163,22 @@ for param in paramList_Sheet:
 			pValue = param.AsString()
 			pList_Sheet.append([pName, pValue])
 
-#=========Params for Title
+# =========Params for Title
 paramList_Title = ElemFrom.Parameters
 for param in paramList_Title:
 	storeType = param.StorageType
-	
-	#check if param is boolean
-	isBool = (storeType == StorageType.Integer and\
-			param.Definition.ParameterType == ParameterType.YesNo)
-	
+
+	# check if param is boolean
+	isBool = (storeType == StorageType.Integer and param.Definition.ParameterType == ParameterType.YesNo)
+
 	if StorageType.String or isBool:
-		#check if parameter Built-in
+		# check if parameter Built-in
 		parBIP = param.Definition.BuiltInParameter.ToString()
 		if parBIP == "INVALID":
 			checkName = param.Definition.Name
 		else:
 			checkName = parBIP
-		
+
 		if checkName not in ignorParamsList:
 			pName = param.Definition.Name
 			if isBool:
@@ -183,7 +187,7 @@ for param in paramList_Title:
 				pValue = param.AsString()
 			pList_Title.append([pName, pValue])
 
-#find Legends on view
+# find Legends on view
 try:
 	allVports = [doc.GetElement(x) for x in ElemFrom_Sheet.GetAllViewports()]
 	allViews = [doc.GetElement(x.ViewId) for x in allVports]
@@ -191,11 +195,11 @@ try:
 	legendViews = [x for x in zip(allViews, allCoords) if x[0].ViewType == ViewType.Legend]
 except:
 	legendViews = None
-	
-#=========Start transaction
+
+# =========Start transaction
 TransactionManager.Instance.EnsureInTransaction(doc)
 
-#Set parameters of sheet
+# Set parameters of sheet
 for param in pList_Sheet:
 	pName = param[0]
 	pValue = param[1]
@@ -204,7 +208,7 @@ for param in pList_Sheet:
 	except:
 		None
 
-#Set parameters of Title block
+# Set parameters of Title block
 for param in pList_Title:
 	pName = param[0]
 	pValue = param[1]
@@ -213,7 +217,7 @@ for param in pList_Title:
 	except:
 		None
 
-#CreateLegends
+# CreateLegends
 if legendViews:
 	for legView in legendViews:
 		viewId = legView[0].Id
@@ -224,10 +228,9 @@ if legendViews:
 				ElemTo_Sheet.Id,
 				viewId,
 				insertPoint)
-		except: None
+		except:
+			None
 
-#=========End transaction
+# =========End transaction
 TransactionManager.Instance.TransactionTaskDone()
-
-
 OUT = legendViews
