350b6f9ae37eb0718fc68b37f23a168655e56c66
IvaSerge <ivaserge@ukr.net>
2021-08-06 09:17:49 +0200

    CopyFromDoc add new script

---
+++ CopyFromDoc.py
@@ -0,0 +1,21 @@
+import clr
+clr.AddReference('RevitAPI')
+import Autodesk
+from Autodesk.Revit.DB import *
+
+import sys
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
+
+import System
+from System import Array
+from System.Collections.Generic import *
+
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+doc = DocumentManager.Instance.CurrentDBDocument
+
+
+OUT = None
