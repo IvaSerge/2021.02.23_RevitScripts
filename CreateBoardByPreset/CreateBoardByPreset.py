26c8cbb5903bc1d16f3aa5129f71a07341c43d6b
IvaSerge <ivaserge@ukr.net>
2021-11-05 15:32:20 +0100

    n_of_poles.Ser()

--- CreateBoardByPreset/CreateBoardByPreset.py
+++ CreateBoardByPreset/CreateBoardByPreset.py
@@ -4,6 +4,7 @@ import sys
 # sys.path.append(r"C:\Program Files\Dynamo 0.8")
 pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
 sys.path.append(pyt_path)
+sys.path.append(IN[0].DirectoryName)  # type: ignore
 
 clr.AddReferenceByName('Microsoft.Office.Interop.Excel, Version=11.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c')
 from Microsoft.Office.Interop import Excel  # type: ignore
@@ -26,18 +27,103 @@ from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
 
 
+def GetParVal(elem, name):
+	value = None
+	# custom parameter
+	param = elem.LookupParameter(name)
+	# check is it a BuiltIn parameter if not found
+	if not(param):
+		param = elem.get_Parameter(GetBuiltInParam(name))
+
+	# get paremeter Value if found
+	try:
+		storeType = param.StorageType
+		# value = storeType
+		if storeType == StorageType.String:
+			value = param.AsString()
+		elif storeType == StorageType.Integer:
+			value = param.AsDouble()
+		elif storeType == StorageType.Double:
+			value = param.AsDouble()
+		elif storeType == StorageType.ElementId:
+			value = param.AsValueString()
+	except:
+		pass
+	return value
+
+
+def GetBuiltInParam(paramName):
+	builtInParams = System.Enum.GetValues(BuiltInParameter)
+	param = []
+	for i in builtInParams:
+		if i.ToString() == paramName:
+			param.append(i)
+			return i
+
+
+def getSystems(_brd):
+	"""Get all systems of electrical board.
+
+		args:
+		_brd - electrical board FamilyInstance
+
+		return list(1, 2) where:
+		1 - main electrical circuit
+		2 - list of connectet low circuits
+	"""
+	allsys = _brd.MEPModel.ElectricalSystems
+	lowsys = _brd.MEPModel.AssignedElectricalSystems
+	if lowsys:
+		lowsysId = [i.Id for i in lowsys]
+		mainboardsysLst = [i for i in allsys if i.Id not in lowsysId]
+		if len(mainboardsysLst) == 0:
+			mainboardsys = None
+		else:
+			mainboardsys = mainboardsysLst[0]
+		lowsys = [i for i in allsys if i.Id in lowsysId]
+		lowsys.sort(key=lambda x: x.CircuitNumber)
+		return mainboardsys, lowsys
+	else:
+		return [i for i in allsys][0], None
+
+
 global doc
 doc = DocumentManager.Instance.CurrentDBDocument
 uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
 uiapp = DocumentManager.Instance.CurrentUIApplication
 app = uiapp.Application
 
+reload = IN[1]  # type: ignore
 
 # get FamilyInstance of the board, that need to be converted
+board_to_convert = UnwrapElement(IN[2])  # type: ignore
+
 # get or hard set sub-panel FamilyType (if need)
 
 # get PanelScheduleView if no view found - create Default
+board_schedule = [x for x in FilteredElementCollector(doc).
+	OfClass(Autodesk.Revit.DB.Electrical.PanelScheduleView).
+	ToElements()
+	if x.TargetId == board_to_convert.Id]
+if board_schedule:
+	board_schedule = board_schedule[0]  # type: Autodesk.Revit.DB.Electrical.PanelScheduleView
+
+
+# =========Start transaction
+TransactionManager.Instance.EnsureInTransaction(doc)
+
 
 # in view create Spare
+board_schedule.AddSpare(1, 1)
+doc.Regenerate()
+last_system = getSystems(board_to_convert)[1][-1]  # Autodesk.Revit.DB.Electrical.ElectricalSystem
+n_of_poles = last_system.get_Parameter(
+	BuiltInParameter.RBS_ELEC_NUMBER_OF_POLES).Set(int(3))
 
 # change Spare parameters according to type
+
+# =========End transaction
+TransactionManager.Instance.TransactionTaskDone()
+
+
+OUT = last_system
