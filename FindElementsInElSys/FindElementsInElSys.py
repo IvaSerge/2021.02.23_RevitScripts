b1a7f028a40ee3c11f2e73617dcd8b00cdaef535
IvaSerge <ivaserge@ukr.net>
2021-12-29 15:45:13 +0100

    sys_elements

--- FindElementsInElSys/FindElementsInElSys.py
+++ FindElementsInElSys/FindElementsInElSys.py
@@ -131,16 +131,59 @@ def get_bip(paramName):
 			return i
 
 
-def get_sys_elements(_el_sys):
+def setup_param_value(elem, name, pValue):
+	# custom parameter
+	param = elem.LookupParameter(name)
+	# check is it a BuiltIn parameter if not found
+	if not(param):
+		try:
+			param = elem.get_Parameter(get_bip(name)).Set(pValue)
+		except:
+			pass
+
+	if param:
+		try:
+			param.Set(pValue)
+		except:
+			pass
+	return elem
+
+
+def get_sys_elements(_el_sys, all_elements=list()):
+	# type: (Autodesk.Revit.DB.Electrical.ElectricalSystem, list()) -> list
+
+	"""Get count of all elements of the system including subsystems\n
+		args:
+			_el_sys - electrical system
+			all_elements - list of elements (used for recursion)
+
+		return:
+			list() - system, count of elements
+	"""
+
 	# get main elements
-	sys_elements = [i for i in _el_sys.Elements]
+	sys_board_name = str(_el_sys.PanelName)
+	sys_nummer = str(_el_sys.CircuitNumber)
+	sys_name_string = sys_board_name + ": " + sys_nummer
+	sys_elements = _el_sys.Elements
 
 	# find if there any electrical board in the system
-	# if the element is board and it's name == upperpanel.circuit numer
-	# pop it out of the list
-	# get all the systems of the board
-	# for every system in the board get elements
-	return sys_elements
+	for elem in sys_elements:
+		# if the element is board and it's name == upperpanel.circuit numer
+		# OST_ElectricalEquipment.Id == -2001040
+		if elem.Category.Id == ElementId(-2001040):
+			board_name = elem.Name
+			# the board is quasi sub-board of the system
+			if board_name == sys_name_string:
+				# get subsystems of the board
+				low_systems = elsys_by_brd(elem)[1]
+				if low_systems:
+					for low_sys in low_systems:
+						get_sys_elements(low_sys, all_elements)
+		else:
+			all_elements.append(elem)
+	elem_count = len(all_elements)
+	return _el_sys, elem_count
 
 
 # ================ GLOBAL VARIABLES
@@ -149,19 +192,52 @@ uiapp = DocumentManager.Instance.CurrentUIApplication
 uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
 app = uiapp.Application
 
-
 reload = IN[1]  # type: ignore
 calc_all = IN[2]  # type: ignore
-outlist = list()
+info_list = list()
 
 # only 1 element to calculate
 if not calc_all:
 	circuits_to_calculate = get_sys_by_selection()
-	sys_elements = get_sys_elements(circuits_to_calculate[0])
 
-# get all electrical systems that are modifiable
+# get all electrical systems
 if calc_all:
-	pass
+	# get all circuits in the model
+	# Get all electrical circuits
+	# Circuit type need to be electrilca only
+	# electrical circuit type ID == 6
+	testParam = BuiltInParameter.RBS_ELEC_CIRCUIT_TYPE
+	pvp = ParameterValueProvider(ElementId(int(testParam)))
+	sysRule = FilterIntegerRule(pvp, FilterNumericEquals(), 6)
+	filter = ElementParameterFilter(sysRule)
+
+	circuits_to_calculate = FilteredElementCollector(doc).\
+		OfCategory(BuiltInCategory.OST_ElectricalCircuit).\
+		WhereElementIsNotElementType().WherePasses(filter).\
+		ToElements()
+
+	voltage_230 = UnitUtils.ConvertToInternalUnits(
+		230, DisplayUnitType.DUT_VOLTS)
+	voltage_400 = UnitUtils.ConvertToInternalUnits(
+		400, DisplayUnitType.DUT_VOLTS)
+
+	circuits_to_calculate = [
+		i for i in circuits_to_calculate
+		if i.Voltage == voltage_230 or i.Voltage == voltage_400
+	]
+
+	# filter out not owned circuits
+	circuits_to_calculate = [
+		i for i in circuits_to_calculate
+		if WorksharingUtils.GetCheckoutStatus(doc, i.Id) != CheckoutStatus.OwnedByOtherUser
+	]
+
+	# Filtering out not connected circuits
+	circuits_to_calculate = [i for i in circuits_to_calculate if i.BaseEquipment]
+
+for circuit in circuits_to_calculate:
+	info = get_sys_elements(circuit, [])
+	info_list.append(info)
 
 # =========Start transaction
 TransactionManager.Instance.EnsureInTransaction(doc)
@@ -170,4 +246,4 @@ TransactionManager.Instance.EnsureInTransaction(doc)
 # =========End transaction
 TransactionManager.Instance.TransactionTaskDone()
 
-OUT = sys_elements
+OUT = info_list
