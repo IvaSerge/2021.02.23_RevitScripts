c5954246535e3eecfa7bf25cfd5909ebbb6d6284
IvaSerge <ivaserge@ukr.net>
2022-02-08 10:02:27 +0100

    uncommented

---
+++ EmLight_Diagramm/EmLight_Diagramm.py
@@ -0,0 +1,10 @@
+# select board to create diagramm
+# select view for diagramm
+
+# No search in deep needed. Find elements by panel name and number
+
+# family, type to 2D matrix
+
+# Calculate insert point
+
+# insert 2D on drawing, add parameters
