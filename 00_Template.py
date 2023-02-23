a6c162545f78b570fb8e8a0d5658c3ccd17f852c
IvaSerge <ivaserge@ukr.net>
2023-02-23 11:14:59 +0100

    dir_path

--- 00_Template.py
+++ 00_Template.py
@@ -4,6 +4,8 @@ import sys
 # sys.path.append(r"C:\Program Files\Dynamo 0.8")
 pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
 sys.path.append(pyt_path)
+dir_path = IN[0].DirectoryName  # type: ignore
+sys.path.append(dir_path)
 
 clr.AddReferenceByName('Microsoft.Office.Interop.Excel, Version=11.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c')
 from Microsoft.Office.Interop import Excel  # type: ignore
