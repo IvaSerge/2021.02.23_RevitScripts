998198cf095a2f06f2fa01eba57ab352e1ae066b
IvaSerge <ivaserge@ukr.net>
2023-02-23 11:08:40 +0100

    first commit

---
+++ ElectricalPanelDiagramm/ElectricalPanelDiagramm.py
@@ -0,0 +1,44 @@
+import clr
+
+import sys
+# sys.path.append(r"C:\Program Files\Dynamo 0.8")
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
+
+clr.AddReferenceByName('Microsoft.Office.Interop.Excel, Version=11.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c')
+from Microsoft.Office.Interop import Excel  # type: ignore
+
+import System
+from System import Array
+from System.Collections.Generic import *
+
+System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo("en-US")
+from System.Runtime.InteropServices import Marshal
+
+# ================ Revit imports
+clr.AddReference('RevitAPI')
+import Autodesk
+from Autodesk.Revit.DB import *
+
+clr.AddReference('RevitAPIUI')
+from Autodesk.Revit.UI import *
+
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+global doc
+doc = DocumentManager.Instance.CurrentDBDocument
+uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
+uiapp = DocumentManager.Instance.CurrentUIApplication
+app = uiapp.Application
+view = doc.ActiveView
+
+# =========Start transaction
+TransactionManager.Instance.EnsureInTransaction(doc)
+
+# =========End transaction
+TransactionManager.Instance.TransactionTaskDone()
+
+OUT = doc
