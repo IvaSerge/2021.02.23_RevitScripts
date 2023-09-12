2983cb64a192ddda105afc24b2a59c2a40718266
IvaSerge <ivaserge@ukr.net>
2023-09-12 23:03:29 +0200

    first commit

---
+++ DataInfoSet/grid.py
@@ -0,0 +1,98 @@
+"""modue to represent architectural grid"""
+import clr
+
+# ================ Revit imports
+clr.AddReference('RevitAPI')
+import Autodesk
+from Autodesk.Revit.DB import *
+
+# ================ Python imports
+import math
+from operator import itemgetter
+
+
+class grid:
+	# class variables
+	all_intersection_points = list()
+
+	@classmethod
+	def find_grid_intersection_points(cls, doc):
+		# type: (Autodesk.Revit.DB.Document) -> list
+		all_grids = FilteredElementCollector(doc).\
+			OfCategory(BuiltInCategory.OST_Grids).\
+			WhereElementIsNotElementType().\
+			ToElements()
+
+		all_intersection_points = dict()
+		obj_grids = [grid(i) for i in all_grids]
+		for grd in obj_grids:
+			grid_intersections = grd.get_intersection_points(obj_grids)
+			all_intersection_points.update(grid_intersections)
+
+		cls.all_intersection_points = all_intersection_points
+		return cls.all_intersection_points
+
+	def __init__(self, _rvt_grid):
+		# type: (Autodesk.Revit.DB.Grid) -> None
+		self.rvt_grid = _rvt_grid  # type: Autodesk.Revit.DB.Grid
+		self.angle = grid.get_angle(_rvt_grid)
+		self.intersections = dict()
+
+	@staticmethod
+	def get_angle(_rvt_grid):
+		# type: (Autodesk.Revit.DB.Grid) -> float
+		"""
+		Get angle rotation fo the grid
+		"""
+		vector = _rvt_grid.Curve.Direction
+		direction = round(math.degrees(math.acos(_rvt_grid.Curve.Direction.X)))
+		# rotating vector in upper direction
+		if direction == 180 or direction == 0:
+			direction = 0
+		elif vector.Y < 0:
+			direction = 180 - direction
+		else:
+			pass
+		return direction
+
+	def get_intersection_points(self, _grids):
+		# type: (grid, list[grid]) -> None
+		grids = [i for i in _grids
+			if i.angle != self.angle]
+
+		curve_current = self.rvt_grid.Curve
+
+		for grid_next in grids:
+			curve_next = grid_next.rvt_grid.Curve
+			# define naming priority
+			# name with smalest angle is first as example ("A" = 0, "1" = 90) -> A1
+			if self.angle < grid_next.angle:
+				point_name = self.rvt_grid.Name + grid_next.rvt_grid.Name
+			else:
+				point_name = grid_next.rvt_grid.Name + self.rvt_grid.Name
+
+			# find intersection point
+			results = Autodesk.Revit.DB.IntersectionResultArray()
+			comparation_result = curve_current.Intersect(curve_next, results)
+			if comparation_result[0] == SetComparisonResult.Overlap:
+				point = comparation_result[1][0].XYZPoint
+				# add point to dictionary
+				self.intersections.update({point_name: point})
+
+		return self.intersections
+
+	@classmethod
+	def get_nearest_grid_by_instance(cls, rvt_inst: FamilyInstance) -> str:
+		"""
+			Get nearest grid string for family
+		"""
+		rvt_inst_point = rvt_inst.Location.Point
+		# # get all distances list
+		distance_list = list()
+		all_intersect_points = cls.all_intersection_points.items()
+		for key, value in all_intersect_points:
+			distance = rvt_inst_point.DistanceTo(value)
+			distance_list.append([key, distance])
+		distance_list.sort(key=itemgetter(1))
+		shortest_grid_name = distance_list[0][0]
+		return shortest_grid_name
---
+++ DataInfoSet/main.py
@@ -0,0 +1,53 @@
+import clr
+
+import sys
+# sys.path.append(r"C:\Program Files\Dynamo 0.8")
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
+
+dir_path = IN[0].DirectoryName  # type: ignore
+sys.path.append(dir_path)
+
+
+# ================ Revit imports
+clr.AddReference('RevitAPI')
+from Autodesk.Revit.DB import *
+
+clr.AddReference('RevitAPIUI')
+from Autodesk.Revit.UI import *
+
+clr.AddReference("RevitServices")
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+# ================ Python imports
+from System import Array
+from System.Collections.Generic import *
+from importlib import reload
+
+# ================ local imports
+import toolsrvt
+reload(toolsrvt)
+from toolsrvt import *
+
+
+# ================ GLOBAL VARIABLES
+doc = DocumentManager.Instance.CurrentDBDocument
+uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
+uiapp = DocumentManager.Instance.CurrentUIApplication
+app = uiapp.Application
+view = doc.ActiveView
+
+# check object category
+# if it is panel - find data circuit and first element of the data circuit
+# other - add to list
+
+# find element parameters
+# nearest grid
+# floor
+
+# find ciruit
+# for circuit find panel info
+
+
+OUT = None
