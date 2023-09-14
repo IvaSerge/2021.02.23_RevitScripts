bdf7d625f4be5074a111c1b674d33b25638c372c
IvaSerge <ivaserge@ukr.net>
2023-09-14 16:09:14 +0200

    get_grid_and_level

--- DataInfoSet/main.py
+++ DataInfoSet/main.py
@@ -24,11 +24,31 @@ from RevitServices.Transactions import TransactionManager
 from System import Array
 from System.Collections.Generic import *
 from importlib import reload
+import re
 
 # ================ local imports
 import toolsrvt
 reload(toolsrvt)
 from toolsrvt import *
+import grid
+reload(grid)
+from grid import *
+
+
+def get_level_name(rvt_elem: Autodesk.Revit.DB.FamilyInstance) -> str:
+	"""
+	Get Level from the element.\\
+	Return string that represents level name according to Naming Standard
+	"""
+	doc = rvt_elem.Document
+	rvt_lvl = doc.GetElement(rvt_elem.LevelId)
+	if not rvt_lvl:
+		raise ValueError("No Host Level found")
+	rvt_level_str = rvt_lvl.Name
+	regexp = re.compile(r"^(.*?)_")
+	check = regexp.match(rvt_level_str)
+	rvt_level_str = check.group(1)
+	return rvt_level_str
 
 
 # ================ GLOBAL VARIABLES
@@ -50,12 +70,6 @@ else:
 		"Selection of two elements")
 	rvt_elem = doc.GetElement(sel_elem.ElementId)
 
-
-# TODO: Info for panel and all elements in panel
-# check object category
-# if it is panel - find data circuit and first element of the data circuit
-# other - add to list
-
 if rvt_elem.Category.Id != -2001040:
 	elem_list = [rvt_elem]
 else:
@@ -65,12 +79,19 @@ else:
 	# other - add to list
 	pass
 
+grid.find_grid_intersection_points(doc)
+
 # find element parameters
-# nearest grid
+params_to_set = list()
+shortest_grid_name = grid.get_nearest_grid_by_instance(rvt_elem)
+params_to_set.append([rvt_elem, "TO Grid", shortest_grid_name])
+elem_level = get_level_name(rvt_elem)
 # floor
 
 # find ciruit
 # for circuit find panel info
 
+# combine circuit and elem parameters to multi-text parameter
 
-OUT = rvt_elem
+
+OUT = elem_level
