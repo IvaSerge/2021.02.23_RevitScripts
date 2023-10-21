98111d18959876a06fbe094a6aa997fe19f3f4ae
IvaSerge <ivaserge@ukr.net>
2023-10-21 21:33:44 +0200

    first commit

---
+++ BoqExportToXLS/BoqExportToSLS.py
@@ -0,0 +1,55 @@
+import clr
+import os
+import sys
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
+
+dir_path = IN[0].DirectoryName  # type: ignore
+sys.path.append(dir_path)
+
+local_data = os.getenv("LOCALAPPDATA")
+dyn_path = r"\python-3.9.12-embed-amd64\Lib"
+py_path = local_data + dyn_path
+sys.path.append(py_path)
+
+# ================ Revit imports
+clr.AddReference('RevitAPI')
+from Autodesk.Revit.DB import *
+
+clr.AddReference('RevitAPIUI')
+from Autodesk.Revit.UI import *
+
+clr.AddReference("RevitServices")
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+# ================ Python imports
+from System import Array
+from System.Collections.Generic import *
+from importlib import reload
+
+# ================ local imports
+import toolsrvt
+reload(toolsrvt)
+from toolsrvt import *
+
+
+# ================ GLOBAL VARIABLES
+doc = DocumentManager.Instance.CurrentDBDocument
+uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
+uiapp = DocumentManager.Instance.CurrentUIApplication
+app = uiapp.Application
+view = doc.ActiveView
+reload_var = IN[1]  # type: ignore
+rvt_elem = IN[2]  # type: ignore
+
+
+# Element selection
+if rvt_elem:
+	rvt_elem = UnwrapElement(rvt_elem)  # type: ignore
+else:
+	sel_elem = uidoc.Selection.PickObject(
+		Autodesk.Revit.UI.Selection.ObjectType.Element,
+		"Selection of two elements")
+
+OUT = None
