64477e62457496c301db3ba257f7eba6bea584e7
IvaSerge <ivaserge@ukr.net>
2023-12-12 12:15:10 +0100

    firrst commit

---
+++ CopyInstanceParameters/CopyInstanceParameters.py
@@ -0,0 +1,52 @@
+"""
+Script copies parameter values from family to other family or list of families.
+Families may be selected inside Dynamo or using API.
+"""
+import clr
+import sys
+
+dir_path = IN[0].DirectoryName  # type: ignore
+sys.path.append(dir_path)
+
+
+# ================ Revit imports
+clr.AddReference('RevitAPI')
+from Autodesk.Revit.DB import *
+
+clr.AddReference('RevitAPIUI')
+from Autodesk.Revit.UI import *
+
+clr.AddReference("RevitServices")
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+# ================ Python imports
+from System import Array
+from System.Collections.Generic import *
+from importlib import reload
+
+# ================ local imports
+import toolsrvt
+reload(toolsrvt)
+from toolsrvt import *
+
+
+# ================ GLOBAL VARIABLES
+doc = DocumentManager.Instance.CurrentDBDocument
+uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
+uiapp = DocumentManager.Instance.CurrentUIApplication
+app = uiapp.Application
+view = doc.ActiveView
+reload_var = IN[1]  # type: ignore
+rvt_elem = IN[2]  # type: ignore
+
+
+# Element selection
+if rvt_elem:
+	rvt_elem = UnwrapElement(rvt_elem)  # type: ignore
+else:
+	sel_elem = uidoc.Selection.PickObject(
+		Autodesk.Revit.UI.Selection.ObjectType.Element,
+		"Selection of two elements")
+
+OUT = None
