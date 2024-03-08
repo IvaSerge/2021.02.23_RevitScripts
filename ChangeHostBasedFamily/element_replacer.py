93388d1e8b9c20a588bf4dfa47af64e289e9e150
IvaSerge <ivaserge@ukr.net>
2024-03-08 13:53:35 +0100

    switch tag

--- ChangeHostBasedFamily/element_replacer.py
+++ ChangeHostBasedFamily/element_replacer.py
@@ -13,6 +13,10 @@ clr.AddReference("RevitServices")
 from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
 
+import System
+from System import Array
+from System.Collections.Generic import *
+
 # ================ local imports
 import toolsrvt
 
@@ -44,7 +48,6 @@ class ElementReplacer:
 			if target_element_id in tag_hosts_ids:
 				tags_associated_with_element.append(tag)
 		self.tags_list = tags_associated_with_element
-		# self.tags_list = tag_collector[0].GetTaggedElementIds()
 
 
 	def create_new_instance(self):
@@ -74,6 +77,32 @@ class ElementReplacer:
 		ElementTransformUtils.RotateElement(doc, family_inst.Id, inst_axes_Z, math.pi / 2)
 		family_inst.Location.Point = ins_pnt
 
-
 		self.new_inst = family_inst
 		return family_inst
+
+	def switch_tags(self):
+		for tag in self.tags_list:  # Assuming self.tags_list is List[IndependentTag]
+			# Remove old reference
+			old_ref_list = tag.GetTaggedReferences()
+			old_ref = old_ref_list[0]
+			tag_visible = tag.IsLeaderVisible(old_ref)
+			tag_end_condition = tag.LeaderEndCondition
+			if tag_visible:
+				tag_elbow = tag.GetLeaderElbow(old_ref)
+			else:
+				tag_elbow = None
+
+			if tag_end_condition == LeaderEndCondition.Free and tag_visible:
+				tag_end = tag.GetLeaderEnd(old_ref)
+			else:
+				tag_end = None
+			tag.RemoveReferences(old_ref_list)
+	
+			# Add reference
+			new_ref = Reference(self.new_inst)
+			new_ref_list = [new_ref]
+			tag.AddReferences(new_ref_list)
+			if tag_elbow:
+				tag.SetLeaderElbow(new_ref, tag_elbow)
+			if tag_end:
+				tag.SetLeaderEnd(new_ref, tag_end)
