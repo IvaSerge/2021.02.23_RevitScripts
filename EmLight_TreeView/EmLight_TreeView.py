95a0fd133b9176913638c915c9a7c96d88cc9c06
IvaSerge <ivaserge@ukr.net>
2024-04-18 23:09:30 +0200

    Create first family instance

--- EmLight_TreeView/EmLight_TreeView.py
+++ EmLight_TreeView/EmLight_TreeView.py
@@ -25,6 +25,9 @@ from importlib import reload
 import toolsrvt
 reload(toolsrvt)
 from toolsrvt import *
+import light_symbol
+reload(light_symbol)
+from light_symbol import *
 
 
 # ================ GLOBAL VARIABLES
@@ -35,7 +38,50 @@ app = uiapp.Application
 view = doc.ActiveView
 reload_var = IN[1]  # type: ignore
 view_name = IN[2]  # type: ignore
-rvt_panel = IN[3]  # type: ignore
+rvt_panel = UnwrapElement(IN[3])  # type: ignore
+LightSymbol.start_point = XYZ(0, 0, 0)
+LightSymbol.get_symbols_types(doc)
+symbols_to_install: list[LightSymbol] = []
+
+# get view
+view_diagramm = inst_by_cat_strparamvalue(
+	doc, 
+	BuiltInCategory.OST_Views,
+	BuiltInParameter.VIEW_NAME,
+	view_name,
+	False)[0]
+
+# get circuits of the panel
+circuits = [i for i in elsys_by_brd(rvt_panel)[1]
+	if i.CircuitType == Autodesk.Revit.DB.Electrical.CircuitType.Circuit]
+
+# test circuit is 48
+# start point to be changed by -y*i
+LightSymbol.current_column = 0
+LightSymbol.current_row = 0
+rvt_circuit = [i for i in circuits if i.StartSlot == 48][0]
+first_elem = LightSymbol.get_first_symbol(rvt_circuit)
+symbols_to_install.append(first_elem)
 
 
-OUT = None
+# =========Start transaction
+TransactionManager.Instance.EnsureInTransaction(doc)
+
+for elem_2D in symbols_to_install:
+	elem_2D.create_2D(view_diagramm)
+	# elem_2D.set_parameters()
+
+# =========End transaction
+TransactionManager.Instance.TransactionTaskDone()
+
+
+# for circuit get all elements
+# sort elements by distance along circuit path
+# find quasi panels
+# calculate insert points for elements
+# calculate length
+# read parameters
+# create 2D symbols
+# insert parameters to 2D
+
+OUT = [i.inst_2D for i in symbols_to_install]
---
+++ EmLight_TreeView/light_symbol.py
@@ -0,0 +1,82 @@
+import clr
+
+# ================ Revit imports
+clr.AddReference('RevitAPI')
+from Autodesk.Revit.DB import *
+
+clr.AddReference('RevitAPIUI')
+from Autodesk.Revit.UI import *
+
+clr.AddReference("RevitServices")
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+
+# ================ local imports
+import toolsrvt
+from toolsrvt import *
+
+class LightSymbol():
+	start_point: XYZ = None
+	current_row: int = 0
+	current_column: int = 0
+	types_list = []
+
+	def __init__(self, _rvt_inst):
+		self.rvt_inst = _rvt_inst
+		self.type_2D: FamilyType = None
+		self.inst_2D: FamilyInstance = None
+		self.insert_point: XYZ = None
+		self.grid_row: int = None
+		self.grid_column: int = None
+
+	@staticmethod
+	def get_first_symbol(_rvt_circuit):
+		first_sym = LightSymbol(None)
+		first_sym.type_2D = first_sym.types_list[0]
+		first_sym.insert_point = first_sym.start_point
+		# get circuit parameters to be written in 2D
+		# get cable type
+		# get cable length
+		# get circuit name
+		return first_sym
+
+	@staticmethod
+	def get_all_symbols_by_circuit():
+		return None
+
+	@classmethod
+	def get_symbols_types(cls, doc):
+
+		type_first = toolsrvt.type_by_bic_fam_type(
+			doc,
+			BuiltInCategory.OST_DetailComponents,
+			"2D_NOT_Steigzonen",
+			"2D_diagramm_NOT_1P")
+
+		type_emergency = toolsrvt.type_by_bic_fam_type(
+			doc,
+			BuiltInCategory.OST_DetailComponents,
+			"2D_NOT_Shema",
+			"2D_diagramm_E01")
+
+		type_exit = toolsrvt.type_by_bic_fam_type(
+			doc,
+			BuiltInCategory.OST_DetailComponents,
+			"2D_NOT_Shema",
+			"2D_diagramm_Exit")
+
+		cls.types_list.append(type_first)
+		cls.types_list.append(type_emergency)
+		cls.types_list.append(type_exit)
+
+	def create_2D(self, rvt_view):
+		insert_pnt = self.insert_point
+		symbol_type: FamilyType = self.type_2D
+		doc = symbol_type.Document
+		instance_on_view = doc.Create.NewFamilyInstance(
+			insert_pnt,
+			symbol_type,
+			rvt_view)
+		self.inst_2D = instance_on_view
+		return instance_on_view
