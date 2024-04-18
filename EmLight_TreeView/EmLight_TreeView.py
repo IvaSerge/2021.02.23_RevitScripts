7286c6c828ff5aaaf40377a4a82a2bcbc4c2f5df
IvaSerge <ivaserge@ukr.net>
2024-04-18 13:25:11 +0200

    first commit

---
+++ EmLight_TreeView/EmLight_TreeView.py
@@ -0,0 +1,41 @@
+import clr
+import sys
+
+dir_path = IN[0].DirectoryName  # type: ignore
+sys.path.append(dir_path)
+
+
+# ================ Revit imports
+clr.AddReference('RevitAPI')
+from Autodesk.Revit.DB import *
+
+clr.AddReference('RevitAPIUI')
+from Autodesk.Revit.UI import *
+
+clr.AddReference("RevitServices")
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+# ================ Python imports
+from System import Array
+from System.Collections.Generic import *
+from importlib import reload
+
+# ================ local imports
+import toolsrvt
+reload(toolsrvt)
+from toolsrvt import *
+
+
+# ================ GLOBAL VARIABLES
+doc = DocumentManager.Instance.CurrentDBDocument
+uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
+uiapp = DocumentManager.Instance.CurrentUIApplication
+app = uiapp.Application
+view = doc.ActiveView
+reload_var = IN[1]  # type: ignore
+view_name = IN[2]  # type: ignore
+rvt_panel = IN[3]  # type: ignore
+
+
+OUT = None
