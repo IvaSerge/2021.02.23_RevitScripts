bebd5d836d29dadbca1e1b5967a18dd05db6124d
IvaSerge <ivaserge@urk.net>
2025-01-30 13:24:47 +0100

    script creates list of panels

--- SystemBrowserToSchedule/rvt_panel.py
+++ SystemBrowserToSchedule/rvt_panel.py
@@ -35,12 +35,20 @@ class RvtPanel():
 	
 	def get_circuts_info(self):
 		outlist = list()
+		if self.rvt_circuits is None:
+			return None
+
 		for circiut in self.rvt_circuits:
+			# fist - get info about current circuti
 			circuit_info = RvtPanel.get_circuit_info(circiut)
+			if not circuit_info:
+				continue
 			outlist.append(circuit_info)
 
-		###	If yes - Create subpanel list
-		### Get ciruit number and circuit name
+			# get info from next panel
+			next_panel_info = RvtPanel.get_next_info(circiut)
+			if next_panel_info:
+				outlist.append(next_panel_info)
 
 		return outlist
 
@@ -49,13 +57,23 @@ class RvtPanel():
 	def get_circuit_info(circuit):
 		# Check if it is a circuit for a panel
 		is_feeder = RvtPanel.check_circuit_is_feeder(circuit)
-		return is_feeder
+
+		# if circuit is nof a feeder - it will be passed
+		if not is_feeder:
+			return None
+	
+		# get info for current circuit
+		circuit_number = circuit.CircuitNumber
+		circuit_name = circuit.LoadName
+
+		return [circuit_number, circuit_name]
 
 	@staticmethod
 	def check_circuit_is_feeder(circuit):
 		# not a spare
 		is_power_sys = circuit.SystemType == Electrical.ElectricalSystemType.PowerCircuit
-		if not is_power_sys:
+		is_circuit_type = circuit.CircuitType == Electrical.CircuitType.Circuit
+		if not is_power_sys or not is_circuit_type:
 			return False
 
 		# not single
@@ -69,15 +87,17 @@ class RvtPanel():
 			return False
 
 		# first element not quasi
-		type_name = first_elem.get_Parameter(BuiltInParameter.SYMBOL_NAME_PARAM).AsString().lower()
+		type_name = first_elem.Symbol.get_Parameter(BuiltInParameter.ALL_MODEL_TYPE_NAME).AsString().lower()
 
 		if "quasi" in type_name:
 			return False
 
 		return True
-	
+
 	@staticmethod
-	def circuti_info_to_string():
-		return None
-
-
+	def get_next_info(circuit):
+		elems = list(circuit.Elements)
+		rvt_next_panel:FamilyInstance = elems[0]
+		next_panel:RvtPanel = RvtPanel(rvt_next_panel)
+		next_panel_info = next_panel.get_circuts_info()
+		return next_panel_info
