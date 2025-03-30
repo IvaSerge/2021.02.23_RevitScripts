92638740632cd7b8a5765f1d1fd95807f699cedd
IvaSerge <ivaserge@urk.net>
2025-03-30 21:22:43 +0200

    BOQ size changed

--- BoqExportToXLS/xl_writer.py
+++ BoqExportToXLS/xl_writer.py
@@ -170,7 +170,7 @@ def write_cable_list(ws, circuits_list):
 	ws.column_dimensions["A"].width = 30
 	ws.column_dimensions["B"].width = 10
 	ws.column_dimensions["C"].width = 40
-	ws.column_dimensions["D"].width = 35
+	ws.column_dimensions["D"].width = 40
 	ws.column_dimensions["E"].width = 10
 	ws.column_dimensions["F"].width = 35
 	ws.column_dimensions["G"].width = 28
